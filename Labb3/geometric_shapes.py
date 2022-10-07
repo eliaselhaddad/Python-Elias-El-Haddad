35684a1796c90fc84e7a413d1b37c6eb51c3e4a9
Elias El Haddad <eliaselhaddad@icloud.com>
2022-10-07 14:47:14 +0200

    labb3

--- Labb3/geometric_shapes.py
+++ Labb3/geometric_shapes.py
@@ -1,25 +1,81 @@
+# from __future__ import ann
 from math import pi
 
 class Circle:
 
-    def __init__(self, x: float, y: float, r: float) -> None:
+    def __init__(self, x: float, y: float, radius: float) -> None:
         self.x = x
         self.y = y
-        self.r = r
+        self.radius = radius
 
     @property
-    def area(self):
-        return pi*self.r**2
+    def x(self) -> float:
+        return self._x
+
+    @x.setter
+    def x(self, value: float) -> None:
+        if not isinstance(value, (float, int)):
+            raise TypeError(f"x must be a float or int not, {type(value)}")
+        self._x = value
 
     @property
-    def circumference(self):
-        return 2*pi*self.r
+    def y(self) -> float:
+        return self._y
 
-    def __eq__(self, other) -> bool:
+    @y.setter
+    def y(self, value: float) -> None:
+        if not isinstance(value, (float, int)):
+            raise TypeError(f"y must be a float or int, not {type(value)}")
+        self._y = value
+
+    @property
+    def radius(self) -> float:
+        return self._radius
+
+    @radius.setter
+    def radius(self, value: float) -> None:
+        if not isinstance(value, (float, int)):
+            raise TypeError(f"radius must be a float or int, not {type(value)}")
+        self._radius = value
+
+    @property
+    def area(self) -> float:
+        return pi*self.radius**2
+
+    @property
+    def circumference(self) -> float:
+        return 2*pi*self.radius
+
+    def __eq__(self, other: object) -> bool:
         if self.area == other.area:
             return True
         else:
             return False
+    def __gt__(self, other) -> bool:
+        if self.area > other.area:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other) -> bool:    
+        if self.area < other.area:
+            return True
+        else:
+            return False
+
+    def __ge__(self, other) -> bool:
+        if self.area >= other.area:
+            return True
+        else:
+            return False
+
+    def __le__(self, other) -> bool:
+        if self.area <= other.area:
+            return True
+        else:
+            return False
+    
+
 
     
 
