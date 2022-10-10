4e35a19baf0f3f2adef09eabcb726b66fec8dcc6
Elias El Haddad <eliaselhaddad@icloud.com>
2022-10-10 16:28:07 +0200

    labb3

--- Labb3/geometric_shapes.py
+++ Labb3/geometric_shapes.py
@@ -1,39 +1,23 @@
 from math import pi
 import matplotlib.pyplot as plt
 
-class Circle:
 
-    def __init__(self, x: float, y: float, radius: float) -> None:
+class Shapes:
+    def __init__(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
-        self.radius = radius
 
         if not isinstance(x, (int, float)):
             raise TypeError("x must be int or float")
         if not isinstance(y, (int, float)):
             raise TypeError("y must be int or float")
-        if not isinstance(radius, (int, float)):
-            raise TypeError("Radius must int or float")
 
-    def __repr__(self) -> str:
-        return f"Circle(x = {self.x}, y = {self.y}, radius = {self.radius})"
+    def __str__(self):
+        return f"Shape with x: {self.x} and y: {self.y}"
 
-    def __str__(self) -> str:
-        pass
+    def __repr__(self):
+        return f"Shape({self.x}, {self.y})"
 
-    @property
-    def area(self) -> float:
-        return pi*self.radius**2
-
-    @property
-    def circumference(self) -> float:
-        return 2*pi*self.radius
-
-    def __eq__(self, other: object) -> bool:
-        if self.area == other.area:
-            return True
-        else:
-            return False
     def __gt__(self, other) -> bool:
         if self.area > other.area:
             return True
@@ -58,23 +42,69 @@ class Circle:
         else:
             return False
 
-
-class Rectangle:
-
-    def __init__(self, x: float, y: float, width: float, height: float) -> None:
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-
+    def translate(self, x, y) -> None:
         if not isinstance(x, (int, float)):
             raise TypeError("x must be int or float")
         if not isinstance(y, (int, float)):
             raise TypeError("y must be int or float")
+        self.x += x
+        self.y += y
+
+
+class Circle(Shapes):
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
+
+        if not isinstance(radius, (int, float)):
+            raise TypeError("Radius must int or float")
+
+    def __repr__(self) -> str:
+        return f"Circle(x = {self.x}, y = {self.y}, radius = {self.radius})"
+
+    def __str__(self) -> str:
+        pass
+
+    @property
+    def area(self) -> float:
+        return pi*self.radius**2
+
+    @property
+    def circumference(self) -> float:
+        return 2*pi*self.radius
+
+    def __eq__(self, other: object) -> bool:
+        if self.area == other.area:
+            return True
+        else:
+            return False
+
+    def is_inside(self, x, y) -> bool:
+        distance = ((self.x - x) ** 2 + (self.y - y) ** 2)
+        if distance <= self.radius:
+            return True
+        else:
+            return False
+
+    def is_circle_unit(self) -> bool:
+        if self.radius == 1:
+            return True
+        else:
+            return False
+
+
+class Rectangle(Shapes):
+
+    def __init__(self, x, y, width, height) -> None:
+        super().__init__(x, y)
+        self.width = width
+        self.height = height
+
         if not isinstance(width, (int, float)):
-            raise TypeError("width must be int or float")
+            raise TypeError("Width must int or float")
         if not isinstance(height, (int, float)):
-            raise TypeError("height must be int or float")
+            raise TypeError("Height must int or float")
 
     @property
     def area(self) -> float:
@@ -83,6 +113,25 @@ class Rectangle:
     @property
     def perimeter(self) -> float:
         return 2*(self.width + self.height)
+
+    def __eq__(self, other: object) -> bool:
+        if self.area == other.area:
+            return True
+        else:
+            return False
+
+    def is_inside(self, x, y) -> bool:
+        distance = ((self.x - x) ** 2 + (self.y - y) ** 2)
+        if distance <= self.width:
+            return True
+        else:
+            return False
+
+    def is_square(self) -> bool:
+        if self.width == self.height:
+            return True
+        else:
+            return False
 
     
 
